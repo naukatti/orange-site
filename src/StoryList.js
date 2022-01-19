c9bdb55b951d51d9a74ce91f9d0cf125a5105f71
naukatti <noora.peltok@gmail.com>
2022-01-19 23:01:34 +0200

    Now shows 20 stories

--- src/StoryList.js
+++ src/StoryList.js
@@ -11,7 +11,7 @@ const StoryList = () => {
     axios
       .get("https://hacker-news.firebaseio.com/v0/beststories.json")
       .then((response) => {
-        let storyIds = response.data.slice(0, 3);
+        let storyIds = response.data.slice(0, 19);
         console.log("response got");
 
         // getting all of the best stories data
